a24e73b5c50dabe197206585c31349266b1eef64
Ihor Avdiunin <igor.avdiunin@gmail.com>
2023-10-26 10:35:41 +0300

    finished goit-js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(handleTimeUpdate, 1000));
+
+function handleTimeUpdate(evt) {
+  const currentTime = evt.seconds;
+
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+const targetTime =
+  parseFloat(localStorage.getItem('videoplayer-current-time')) ?? 0;
+
+player.setCurrentTime(targetTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(evt) {
+  evt.preventDefault();
+  const emailEl = formEl.elements.email.value;
+  const messageEl = formEl.elements.message.value;
+  const inputValue = {
+    email: emailEl,
+    message: messageEl,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputValue));
+}
+
+const targetValue = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (targetValue) {
+  formEl.elements.email.value = targetValue.email;
+  formEl.elements.message.value = targetValue.message;
+}
